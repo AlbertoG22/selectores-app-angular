e62e2abc5b8c0e813d239739a198e4d5bffe2f8c
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-24 19:52:33 -0600

    setting app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SelectorPageComponent } from './selector/pages/selector-page/selector-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectorPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
-
-
+import { CountriesRoutingModule } from './countries-routing.module';
 
 @NgModule({
   declarations: [
     SelectorPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRoutingModule
   ]
 })
 export class CountriesModule { }
