08316555c9118b907c98373b259fddcfe3d0233d
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-29 19:53:46 -0600

    get countries by region

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Region } from '../../interfaces/country.interfaces';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -30,6 +31,9 @@ export class SelectorPageComponent implements OnInit {
 
   onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
+      .pipe(
+        switchMap( region => this.countriesService.getCountriesByRegion(region) )
+      )
       .subscribe( region => {
         console.log({ region });
       });
