d50ae7b7d53888150e567b7002e9a3108cfc09cd
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-07-29 19:19:52 -0600

    countries field - second selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Region } from '../../interfaces/country.interfaces';
 import { CountriesService } from '../../services/countries.service';
@@ -8,20 +8,32 @@ import { CountriesService } from '../../services/countries.service';
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
-  constructor(
-    private fb: FormBuilder,
-    private countriesService: CountriesService
-  ) {}
-
-  get regions(): Region[] {
-    return this.countriesService.regions;
-  }
-
+export class SelectorPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     region: [ '', Validators.required ],
     country: [ '', Validators.required ],
     borders: [ '', Validators.required ],
   });
+
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChanged(): void {
+    this.myForm.get('region')!.valueChanges
+      .subscribe( region => {
+        console.log({ region });
+      });
+  }
+
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,10 @@ export class CountriesService {
 
   get regions() : Region[] {
     return [ ...this._regions ];
+  }
+
+  getCountriesByRegion( region: Region ): SmallCountry[] {
+
+    return [];
   }
 }
